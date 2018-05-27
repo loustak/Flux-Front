8d4edc116b33746f49ccac03b0075ace6a2a2f62
Loustak <sardois.lucas@gmail.com>
2018-05-27 21:54:49 +0200

    Initial commit for the front end.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// To use JSX syntax
+import React from 'react';
+// To use ReactDOM
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello World</h1>,
+  document.getElementById('root')
+);
