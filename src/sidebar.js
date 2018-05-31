c5a8926975ce4f86f027d04ac0db437286dd3f59
Loustak <sardois.lucas@gmail.com>
2018-05-31 09:34:27 +0200

    feat(homeAuth/communities): The list of communities is now filled. Added a button to join a community

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import './override.css'
+import { Nav, NavItem, NavLink, Modal, ModalHeader, ModalBody, ModalFooter, Button, Input, Form } from 'reactstrap';
 
 export default class SideBar extends React.Component {
 
@@ -19,8 +17,8 @@ class CommunitySideBar extends React.Component {
 
   constructor(props) {
     super(props);
-
-    // Fetch all the comunity of the users
+    this.state = {communities: []};
+    this.getUserCommunities = this.getUserCommunities.bind(this);
     this.getUserCommunities();
   }
 
@@ -36,7 +34,7 @@ class CommunitySideBar extends React.Component {
     .then(
       (result) => {
         if (result.success) {
-          console.log(result);
+          this.setState({communities: result.communities});
         } else {
           this.setState({finished: true, error: true});
         }
@@ -48,16 +46,116 @@ class CommunitySideBar extends React.Component {
   }
   
   render() {
+    var communities = [];
+    var i = 0;
+    this.state.communities.map((community) => {
+      communities.push(
+        <Community key={i} name={community.name} />
+      );
+    });
+
     return(
       <div className="community-sidebar-wrapper">
         <Nav>
           <NavItem>
-            <NavLink href="/">1</NavLink>
-            <NavLink href="/">2</NavLink>
-            <NavLink href="/">3</NavLink>
+            {communities}
+            <CommunityJoinButton token={this.props.token} onCommunityChange={this.getUserCommunities}/>
           </NavItem>
         </Nav>
       </div>
+    )
+  }
+}
+
+class Community extends React.Component {
+
+  render() {
+    return (
+      <NavLink href="/">{this.props.name}</NavLink>
+    )
+  }
+}
+
+class CommunityJoinButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Button onClick={this.toggle}>Join</Button>
+        <CommunityJoinForm token={this.props.token} isOpen={this.state.modal} 
+          onCommunityChange={this.props.onCommunityChange}
+          toggle={this.toggle} className={this.props.className} />
+      </React.Fragment>
+    )
+  }
+}
+
+class CommunityJoinForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {communityId: ''};
+    this.changeHandler = this.changeHandler.bind(this);
+    this.joinHandler = this.joinHandler.bind(this);
+  }
+
+  changeHandler(event) {
+    this.setState({communityId: event.target.value});
+  }
+
+  joinHandler() {
+    fetch(process.env.REACT_APP_API_PATH + '/communities/' + this.state.communityId + '/join', {
+      method: 'post',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer: ' + this.props.token,
+      }
+    })
+    .then(res => res.json())
+    .then(
+      (result) => {
+        if (result.success) {
+          console.log("Community joined");
+          this.props.onCommunityChange();
+          this.props.toggle();
+        } else {
+          console.log("Error");
+        }
+      },
+
+      (error) => {
+        console.log("Error");
+      });
+  }
+
+  render() {
+    return(
+      <Modal centered isOpen={this.props.isOpen} toggle={this.props.toggle} className={this.props.className}>
+        <ModalHeader toggle={this.props.toggle}>Join a community</ModalHeader>
+        <ModalBody>
+            <Form>
+              <Input placeholder="Community name" onChange={this.changeHandler} />
+            </Form>
+        </ModalBody>
+        <ModalFooter>
+            <Button color="secondary" onClick={this.joinHandler}>Join</Button>
+        </ModalFooter>
+      </Modal>
     )
   }
 }
