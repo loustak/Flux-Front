09b5aa465f5f2cdf3a92788cf55a3c665199ebe0
Loustak <sardois.lucas@gmail.com>
2018-06-03 16:56:08 +0200

    feat(UI): Logo on the home page

--- src/home-visitor.js
+++ src/home-visitor.js
@@ -4,18 +4,21 @@ import './override.css';
 import { Jumbotron } from 'reactstrap';
 
 export class HomeVisitor extends React.Component {
-  //<img src={process.env.PUBLIC_URL + 'security.svg'} className="pull-left vertical-align" />
 
   render() {
     return (
       <Jumbotron fluid>
         <div className="container-fluid flux-container">
-          <h1 className="display-3 text-uppercase font-weight-bold">flux</h1>
-          <p className="lead">A fast an light unified exchange.</p>
+          <div>
+            <h1 id="text-logo" className="display-3 text-uppercase font-weight-bold">flux</h1>
+            <img id="picture-logo" alt="Flux logo" src={process.env.PUBLIC_URL + '/logo_64.png'} />
+          </div>
+          <div>
+            <p className="lead">A fast an light unified exchange.</p>
 
-          <p>Flux is made to be as secure as possible. We use the latest cryptographics 
-          algorithms to encode your data from end to end in the most secure way.</p>
-
+            <p>Flux is made to be as secure as possible. We use the latest cryptographics 
+            algorithms to encode your data from end to end in the most secure way.</p>
+          </div>
         </div>
       </Jumbotron>
     )
